fc5617249e0f91b42d75d5815b24ecefa783e0ce
greg <gregbugaj@yahoo.com>
2020-07-13 07:29:42 -0500

    Add array-pattern tests

--- src/index.ts
+++ src/index.ts
@@ -371,9 +371,10 @@ if (false) {
         })
 }
 
+
 if (true) {
-        const code = 'try {} catch ([a,b, {c, d:e=0, [f]:g=0, h=i}]) {}'
-        const name = 'patterned-catch'
+        const code = 'let [...[x]] = y'
+        const name = 'rest-element-array-pattern'
         const dir = resolve(__dirname, '../test/fixtures', 'ES6', 'binding-pattern', 'array-pattern')
 
         const ast = ASTParser.parse({ type: "code", value: code });
@@ -387,12 +388,12 @@ if (true) {
         const jsonFile = resolve(dir, `${name}.tree.json`)
 
         
-     /*    if (fs.existsSync(jsFile)) {
+/*         if (fs.existsSync(jsFile)) {
                 throw new Error('File exists')
-        } */
+        } 
 
         Utils.write(jsFile, code)
-        Utils.write(jsonFile, ast)
+        Utils.write(jsonFile, ast) */
 }
 // Trick to prevent  
 // All files must be modules when the '--isolatedModules' flag is provided.ts(1208)
