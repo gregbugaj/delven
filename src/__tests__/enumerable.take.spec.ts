b5420924ea316c48a5e9bb3674827076b6ad1809
Greg <gregbugaj@yahoo.com>
2021-02-05 06:21:36 -0600

    Add test for iterator

--- src/__tests__/enumerable.take.spec.ts
+++ src/__tests__/enumerable.take.spec.ts
@@ -4,10 +4,22 @@ import { Enumerable } from "../query/Enumerable";
 describe("Enumerable Take", () => {
     beforeAll(() => { });
 
-    test("take-001", async () => {
+    test("take-toArray()", async () => {
         let enumerable = new Enumerable([0, 1, 2, 3, 4, 5, 6, 7, 8])
         let t3 = await enumerable.Take(2).toArray()
         let expectedResult = [0, 1]
         expect(t3).toEqual(expectedResult);
+    });
+
+
+    test("take-iterator", async () => {
+        const enumerable = new Enumerable([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        const expectedResult = [0, 1]
+        const results = []
+        for await (const val of enumerable.Take(2)) {
+            results.push(val)
+        }
+
+        expect(results).toEqual(expectedResult);
     });
 });
