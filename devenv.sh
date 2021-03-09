224379b8cc4e97d294d300f70b7e046fc17f18ca
greg <gregbugaj@yahoo.com>
2021-03-09 08:11:34 -0600

    Fix issue with starting the environment
    TCP/TCP6 are now parsed correctly
    npm link fixed

--- devenv.sh
+++ devenv.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function log() {
-    if [[ $# != 1 && $# != 3 ]]; then 
+    if [[ $# != 1 && $# != 3 ]]; then
         printf '\e[1;91m%-6s\e[0m\n' "Invalid number of arguments, got $#  but expected 1 or 3"
         return
     fi
 
     label=""
-    if [ "$#" == 1 ]; then        
+    if [ "$#" == 1 ]; then
         tag="INFO"
         msg="$1"
     else
@@ -23,7 +23,7 @@ function log() {
         printf '[\e[1;91m ERROR\e[0m ] [%-6s] %-6s\n' "$trimmed" "$msg"
     else
         printf '[\e[1;93m INFO \e[0m ] [%-6s] %-6s\n' "$trimmed" "$msg"
-    fi 
+    fi
 }
 
 function cleanup()
@@ -35,7 +35,7 @@ trap cleanup EXIT
 
 log "Starting Explorer Environment"
 # Verfiy that the node is present
-if ! command -v tmux &> /dev/null 
+if ! command -v tmux &> /dev/null
 then
     log "ERROR" "Service Tag" "Message"
     printf '\e[1;31m%-6s\e[m\n' "Command 'tmux' not found, but can be installed with:"
@@ -49,12 +49,12 @@ then
         case $opt in
             "Install via 'apt'")
                 printf '\e[1;32m%-6s\e[m\n' "Installing via apt"
-                sudo apt  install tmux 
+                sudo apt  install tmux
                 break
                 ;;
             "Install via 'snap'")
                 printf '\e[1;32m%-6s\e[m\n' "Installing via snap"
-                sudo snap install tmux 
+                sudo snap install tmux
                 break
                 ;;
             "Quit")
@@ -77,11 +77,11 @@ expected_port_ws_server=5000
 # Can also use ss util from
 # ss -l -p -n | grep 3000
 
-port_ui_dev=$(netstat --all --listening --numeric --tcp | awk '{split($0, a,":"); print a[4]}' | grep $expected_port_ui_dev | xargs)
-port_ui_server=$(netstat --all --listening --numeric --tcp | awk '{split($0, a,":"); print a[4]}' | grep $expected_port_ui_server | xargs)
-port_ws_server=$(netstat --all --listening --numeric --tcp | awk '{split($0, a,":"); print a[4]}' | grep $expected_port_ws_server | xargs)
+port_ui_dev=$(netstat --all --listening --numeric --tcp | awk '{split($0, a," "); print a[4]}' | awk '{split($0, a,":"); print a[2]}' | grep $expected_port_ui_dev | xargs)
+port_ui_server=$(netstat --all --listening --numeric --tcp | awk '{split($0, a," "); print a[4]}' | awk '{split($0, a,":"); print a[2]}' | grep $expected_port_ui_server | xargs)
+port_ws_server=$(netstat --all --listening --numeric --tcp | awk '{split($0, a," "); print a[4]}' | awk '{split($0, a,":"); print a[2]}' | grep $expected_port_ws_server | xargs)
 
-hasErrors=0 
+hasErrors=0
 if [ "$port_ui_dev" == "$expected_port_ui_dev" ]; then
     log "ERROR" "Explorer UI" "Port #$port_ui_dev already in use"
     hasErrors=1
@@ -102,28 +102,31 @@ if [ $hasErrors == 1 ]; then
     exit 1
 fi
 
+log "Preflight check complete"
+
 # setup shared development using `npm link`
 ## This is somewhat hacky but because we are using TypeScript it is necessary
 ## 1) Precompile
-## 2) Copy package.json : This has to happen as there is an issue with 
+## 2) Copy package.json : This has to happen as there is an issue with
 ##    'npm link' backtracking a directory down and using that as the node_module package
 ##    and not the 'dist' folder
 ## 3) link dist directory
 ## 3) link destincation directory via 'npm link'
 
 
-# log "Compiling / Linking shared libs"
+log "Compiling / Linking shared libs"
 
-# cd ./transpiler 
-# npx babel src --out-dir dist --extensions '.ts,.js' --source-maps inline
-# cd ./dist
-# cp ../package.json .
-# npm link 
+cd ./transpiler
+npx babel src --out-dir dist --extensions '.ts,.js' --source-maps inline
+cd ./dist
+cp ../package.json .
+npm link
 
-# cd ../runner/executors
-# npm link delven-transpiler
+cd ../../runner/executors/
+npm link delven-transpiler
 
-# cd ../..
+cd ../..
+log "Transpiler linked"
 
 pane_cmd_1='cd ./explorer-ui && npm run start'
 pane_cmd_2='cd ./runner/executors && npm run dev'
@@ -147,31 +150,31 @@ tmux -f devenv-tmux.conf new-session -d -s "$session" # -d "/usr/bin/env bash -i
 tmux rename-window 'Devenv'
 
 # setup layout
-tmux split-pane -h 
+tmux split-pane -h
 tmux select-pane -t ${session}:${window}.1
-tmux split-pane -v 
+tmux split-pane -v
 
 tmux select-pane -t ${session}:${window}.3
-tmux split-pane -v 
+tmux split-pane -v
 
 tmux select-pane -t ${session}:${window}.4
-tmux split-pane -v 
-tmux split-pane -v 
+tmux split-pane -v
+tmux split-pane -v
 
 tmux select-pane -t ${session}:${window}.3
 tmux resize-pane -D -t ${session}:${window}.3 10 # (Resizes the current pane down by 10 cells)
 
-tmux send -t ${session}:${window}.1 "$pane_cmd_1" ENTER;                
-tmux send -t ${session}:${window}.2 "$pane_cmd_2" ENTER;                 
-tmux send -t ${session}:${window}.3 "$pane_cmd_3" ENTER;                 
-tmux send -t ${session}:${window}.4 "$pane_cmd_4" ENTER;             
-tmux send -t ${session}:${window}.5 "$pane_cmd_5" ENTER;                 
-tmux send -t ${session}:${window}.6 "$pane_cmd_6" ENTER;        
+tmux send -t ${session}:${window}.1 "$pane_cmd_1" ENTER;
+tmux send -t ${session}:${window}.2 "$pane_cmd_2" ENTER;
+tmux send -t ${session}:${window}.3 "$pane_cmd_3" ENTER;
+tmux send -t ${session}:${window}.4 "$pane_cmd_4" ENTER;
+tmux send -t ${session}:${window}.5 "$pane_cmd_5" ENTER;
+tmux send -t ${session}:${window}.6 "$pane_cmd_6" ENTER;
 
 # Focus and attach
 tmux select-pane -t ${session}:${window}.1
 tmux attach-session -t "$session"
- 
+
 # Usefull commands
 # tmux list-panes -a
 # tmux kill-session -t devenv-explorer
