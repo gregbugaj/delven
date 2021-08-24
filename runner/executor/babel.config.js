1dc0d3779216d4b322a95982ae4240bd7d513828
greg <gregbugaj@yahoo.com>
2021-08-24 07:16:59 -0500

    Fix issu with warning during docker build

--- runner/executor/babel.config.js
+++ runner/executor/babel.config.js
@@ -9,8 +9,8 @@ module.exports = {
     ],
 
     plugins: [
-        "@babel/plugin-proposal-nullish-coalescing-operator",
-        "@babel/plugin-proposal-optional-chaining",
+        ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": true }],
+        ["@babel/plugin-proposal-optional-chaining", { "loose": true }],
         ["@babel/plugin-proposal-class-properties",
             { "loose": true }],
         ["@babel/transform-runtime"],
