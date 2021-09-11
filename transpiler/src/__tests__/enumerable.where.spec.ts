bc36d8c30ad846d37d96cc07e97da83bf8e75d0f
greg <gregbugaj@yahoo.com>
2021-09-11 12:36:08 -0500

    Fix test

--- transpiler/src/__tests__/enumerable.where.spec.ts
+++ transpiler/src/__tests__/enumerable.where.spec.ts
@@ -1,35 +1,34 @@
 import {Enumerable} from "../query/internal"
 
 describe("Enumerable Where", () => {
-    beforeAll(() => {})
+    beforeAll(() => {
+    })
 
-    if (false)
-        test("where-toArray()", async () => {
-            let queryWhere = new Enumerable([1, 2, "A", 1, 2, 3, 2, 3])
-            let where = queryWhere.Where((val: string | number) => {
-                return val === "A"
-            })
-            let result = await where.toArray()
-            let expectedResult = ["A"]
-
-            expect(result).toEqual(expectedResult)
+    test("where-toArray()", async () => {
+        let queryWhere = new Enumerable([1, 2, "A", 1, 2, 3, 2, 3])
+        let where = queryWhere.Where((val: string | number) => {
+            return val === "A"
         })
+        let result = await where.toArray()
+        let expectedResult = ["A"]
 
-    if (false)
-        test("where-iterator-async", async () => {
-            let queryWhere = new Enumerable([1, 2, "A", 1, 2, 3, 2, 3])
-            let where = queryWhere.Where((val: string | number) => {
-                return val === 2
-            })
-            let expectedResult = [2, 2, 2]
-            let results = []
+        expect(result).toEqual(expectedResult)
+    })
 
-            for await (const val of where) {
-                results.push(val)
-            }
-
-            expect(results).toEqual(expectedResult)
+    test("where-iterator-async", async () => {
+        let queryWhere = new Enumerable([1, 2, "A", 1, 2, 3, 2, 3])
+        let where = queryWhere.Where((val: string | number) => {
+            return val === 2
         })
+        let expectedResult = [2, 2, 2]
+        let results = []
+
+        for await (const val of where) {
+            results.push(val)
+        }
+
+        expect(results).toEqual(expectedResult)
+    })
 
     test("where-iterator-composite", async () => {
         let queryWhere = new Enumerable([1, 2, "A", 1, 2, 3, 2, 3])
