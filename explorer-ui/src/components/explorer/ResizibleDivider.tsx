9af059cb56c6a9f4d62d0db72c0ef8c10676805f
Greg Bugaj <gregbugaj@yahoo.com>
2022-01-13 17:03:12 -0600

    WIP

--- explorer-ui/src/components/explorer/ResizibleDivider.tsx
+++ explorer-ui/src/components/explorer/ResizibleDivider.tsx
@@ -3,101 +3,119 @@ import React from 'react'
 // ref : https://htmldom.dev/create-resizable-split-views/
 export default function ResizibleDivider(props: { direction: "vertical" | "horizontal" }) {
 
-  const { direction } = props
-  const resizerRef = React.createRef<HTMLDivElement>();
-  const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
+    const {direction} = props
+    const resizerRef = React.createRef<HTMLDivElement>();
+    const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
 
-  // The current position of mouse
-  let x = 0;
-  let y = 0;
+    // The current position of mouse
+    let x = 0;
+    let y = 0;
 
-  let prevSiblingWidth = 0;
-  let prevSiblingHeight = 0;
+    let prevSiblingWidth = 0;
+    let prevSiblingHeight = 0;
 
-  const handleMouseDown = (event) => {
-    const resizer = resizerRef.current;
-    const prevSibling = resizer.previousElementSibling as HTMLDivElement;
-    // Get the current mouse position
-    x = event.clientX;
-    y = event.clientY;
+    const handleMouseDown = (event) => {
+        const resizer = resizerRef.current;
 
-    const rect = prevSibling.getBoundingClientRect()
-    prevSiblingWidth = rect.width;
-    prevSiblingHeight = rect.height;
+        if (resizer === null) {
+            return
+        }
 
-    // Attach the listeners to `document`
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-  }
+        const prevSibling = resizer.previousElementSibling as HTMLDivElement;
+        // Get the current mouse position
+        x = event.clientX;
+        y = event.clientY;
 
-  const handleMouseUp = (event) => {
-    const resizer = resizerRef.current;
-    const prevSibling = resizer.previousElementSibling as HTMLDivElement;
-    const nextSibling = resizer.nextElementSibling as HTMLDivElement;
-    console.info('message: "Mouse UP"')
+        const rect = prevSibling.getBoundingClientRect()
+        prevSiblingWidth = rect.width;
+        prevSiblingHeight = rect.height;
 
-    // resizer.style.removeProperty('cursor');
-    document.body.style.removeProperty('cursor');
-    resizer.style.cursor = cursor
-
-    prevSibling.style.removeProperty('user-select');
-    prevSibling.style.removeProperty('pointer-events');
-
-    nextSibling.style.removeProperty('user-select');
-    nextSibling.style.removeProperty('pointer-events');
-
-    // Remove the handlers of `mousemove` and `mouseup`
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseup', handleMouseUp);
-  }
-
-  const handleMouseMove = (event) => {
-    // console.info('message: "Mouse Move"')
-    const resizer = resizerRef.current;
-    const prevSibling = resizer.previousElementSibling as HTMLDivElement;
-    const nextSibling = resizer.nextElementSibling as HTMLDivElement;
-
-    // How far the mouse has been moved
-    const dx = event.clientX - x;
-    const dy = event.clientY - y;
-
-    switch (direction) {
-      case 'vertical':
-        const h = (prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height;
-        prevSibling.style.height = `${h}%`;
-        break;
-      case 'horizontal':
-        const w = (prevSiblingWidth + dx) * 100 / resizer.parentNode.getBoundingClientRect().width;
-        prevSibling.style.width = `${w}%`;
-        break;
+        // Attach the listeners to `document`
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
     }
 
-    // Fix flickering issue
-    resizer.style.cursor = cursor;// 'col-resize';
-    document.body.style.cursor = cursor; //'col-resize';
+    const handleMouseUp = (event) => {
+        const resizer = resizerRef.current;
+        if (resizer === null) {
+            return
+        }
 
-    prevSibling.style.userSelect = 'none';
-    prevSibling.style.pointerEvents = 'none';
+        const prevSibling = resizer.previousElementSibling as HTMLDivElement;
+        const nextSibling = resizer.nextElementSibling as HTMLDivElement;
+        console.info('message: "Mouse UP"')
 
-    nextSibling.style.userSelect = 'none';
-    nextSibling.style.pointerEvents = 'none';
-  }
+        // resizer.style.removeProperty('cursor');
+        document.body.style.removeProperty('cursor');
+        resizer.style.cursor = cursor
 
-  const horizontalContainer = (
-    <div
-      style={{ border: "0px solid red", cursor: cursor, backgroundColor: "#cbd5e0", width: '4px', height: '100%' }}
-      ref={resizerRef}
-      onMouseDown={handleMouseDown}>
-    </div>
-  )
+        prevSibling.style.removeProperty('user-select');
+        prevSibling.style.removeProperty('pointer-events');
 
-  const verticalContainer = (
-    <div
-      style={{ border: "0px solid red", cursor: cursor, backgroundColor: "#cbd5e0", width: '100%', height: '4px' }}
-      ref={resizerRef}
-      onMouseDown={handleMouseDown}>
-    </div>
-  )
+        nextSibling.style.removeProperty('user-select');
+        nextSibling.style.removeProperty('pointer-events');
 
-  return direction === 'horizontal' ? horizontalContainer : verticalContainer
+        // Remove the handlers of `mousemove` and `mouseup`
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    }
+
+    const handleMouseMove = (event) => {
+        // console.info('message: "Mouse Move"')
+        const resizer = resizerRef.current;
+        if (resizer === null) {
+            return
+        }
+        const prevSibling = resizer.previousElementSibling as HTMLDivElement;
+        const nextSibling = resizer.nextElementSibling as HTMLDivElement;
+
+        // How far the mouse has been moved
+        const dx = event.clientX - x;
+        const dy = event.clientY - y;
+
+        if (resizer.parentNode == null) {
+            return
+        }
+
+        switch (direction) {
+            case 'vertical':
+                // @ts-ignore
+                const h = (prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height;
+                prevSibling.style.height = `${h}%`;
+                break;
+            case 'horizontal':
+                // @ts-ignore
+                const w = (prevSiblingWidth + dx) * 100 / resizer.parentNode.getBoundingClientRect().width;
+                prevSibling.style.width = `${w}%`;
+                break;
+        }
+
+        // Fix flickering issue
+        resizer.style.cursor = cursor;// 'col-resize';
+        document.body.style.cursor = cursor; //'col-resize';
+
+        prevSibling.style.userSelect = 'none';
+        prevSibling.style.pointerEvents = 'none';
+
+        nextSibling.style.userSelect = 'none';
+        nextSibling.style.pointerEvents = 'none';
+    }
+
+    const horizontalContainer = (
+        <div
+            style={{border: "1px solid red", cursor: cursor, backgroundColor: "#cbd5e0", width: '4px', height: '100%'}}
+            ref={resizerRef}
+            onMouseDown={handleMouseDown}>
+        </div>
+    )
+
+    const verticalContainer = (
+        <div
+            style={{border: "0px solid red", cursor: cursor, backgroundColor: "#cbd5e0", width: '100%', height: '4px'}}
+            ref={resizerRef}
+            onMouseDown={handleMouseDown}>
+        </div>
+    )
+
+    return direction === 'horizontal' ? horizontalContainer : verticalContainer
 }
--- explorer-ui/src/components/stage/Stage.tsx
+++ explorer-ui/src/components/stage/Stage.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import { EuiPanel, EuiCode, EuiSpacer } from '@elastic/eui';
+
+import {EuiText, EuiResizableContainer} from '@elastic/eui';
+import ResizibleDivider from "../explorer/ResizibleDivider";
 
 function Stage({
                    children,
@@ -9,10 +13,46 @@ function Stage({
     const classes = classNames("euiComponent", "content-stage");
 
     return (
-        <div className={classes}>
-            Content Window Panel<br/>
-            RenderTime : {Date.now()}
+        <>
+        <div style={{display: "flex", height: '100px', border: "2px solid blue", minWidth:'200px'}}>
+            <div style={{display: "flex", width: "50%"}}>Left</div>
+            <ResizibleDivider direction="horizontal"/>
+            <div style={{display: "flex", flex: "1 1 0%", border: "2px solid pink"}}>Right</div>
         </div>
+
+            <div>
+                <div  style={{display: "flex", width: "50%"}}>
+                    LHS
+                </div>
+
+                <div   style={{display: "flex", flex: "1 1 0%", border: "2px solid pink"}}>
+                    RHS
+                </div>
+
+            </div>
+        </>
+
+
+        // <EuiResizableContainer>
+        //     {(EuiResizablePanel, EuiResizableButton) => (
+        //         <>
+        //             <EuiResizablePanel initialSize={50} minSize="200px">
+        //                 <div className={classes}>
+        //                     Content Window Panel<br/>
+        //                     RenderTime : {Date.now()}
+        //                 </div>
+        //
+        //             </EuiResizablePanel>
+        //
+        //             <EuiResizableButton/>
+        //
+        //             <EuiResizablePanel initialSize={50} minSize="200px">
+        //               Right Panel
+        //             </EuiResizablePanel>
+        //         </>
+        //     )}
+        // </EuiResizableContainer>
+
     );
 }
 
