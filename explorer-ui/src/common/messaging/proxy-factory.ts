f959bae6a8ebc82f23a4238e2c87509b6a975451
Greg <gregbugaj@yahoo.com>
2022-02-17 23:09:41 -0600

    Fix linting issues

--- explorer-ui/src/common/messaging/proxy-factory.ts
+++ explorer-ui/src/common/messaging/proxy-factory.ts
@@ -240,6 +240,7 @@ export class JsonRpcProxyFactory<T extends object> implements ProxyHandler<T> {
                     try {
                         if (isNotify) {
                             connection.sendNotification(method, ...args);
+                            // @ts-ignore
                             resolve();
                         } else {
                             const resultPromise = connection.sendRequest(method, ...args) as Promise<any>;
